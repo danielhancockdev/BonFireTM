6431b5a22d2d25b170d648f7ebff79385c23f465
danielhancockdev <danielhancock.dev@gmail.com>
2026-07-23 11:08:51 +0100

    Working on Project Repo Interface

---
+++ Bonfire.Application/Interfaces/IProjectRepository.cs
@@ -0,0 +1,18 @@
+﻿using Bonfire.Domain.Entities;
+
+
+namespace Bonfire.Application.Interfaces
+{
+    public interface IProjectRepository
+    {
+        Task<IEnumerable<Project>> GetAllAsync();
+
+        Task<Project?> GetByIdAsync(Guid id);
+
+        Task AddAsync(Project project);
+
+        Task UpdateAsync(Project project);
+
+        Task DeleteAsync(Project project);
+    }
+}
---
+++ Bonfire.Infrastructure/Repositories/ProjectRepository.cs
@@ -0,0 +1,40 @@
+﻿using Bonfire.Application.Interfaces;
+using Bonfire.Domain.Entities;
+using Bonfire.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bonfire.Infrastructure.Repositories
+{
+    public class ProjectRepository : IProjectRepository
+    {
+        private readonly BonfireDbContext _context;
+
+        public ProjectRepository(BonfireDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Project>> GetAllAsync()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+        public Task<Project?> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Project project)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
